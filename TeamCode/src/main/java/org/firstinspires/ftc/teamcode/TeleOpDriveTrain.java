3551a2401ddb1fad3237d07cc5415f1ca264b059
CalebStanziano <90347719+CalebStanziano@users.noreply.github.com>
2022-10-05 16:43:53 -0400

    10/5

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriveTrain.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp (name = "DriveTrainTeleOp")
+public class TeleOpDriveTrain extends LinearOpMode {
+
+    Hardware robot = Hardware.getInstance();
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        robot.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        //wait for driver to press start
+        waitForStart();
+        boolean xKey = true;
+        boolean xPressed = false;
+
+        while (opModeIsActive()){
+            double forward;
+            double strafing;
+            double turning;
+            double spinTurretX;
+            double spinTurretY;
+
+
+            forward = gamepad1.left_stick_y;
+            turning  = -gamepad1.right_stick_x;
+            strafing = -gamepad1.left_stick_x;
+            spinTurretX = gamepad2.right_stick_x;
+            spinTurretY = gamepad2.right_stick_y;
+
+            //Slow Drive
+            if(gamepad1.dpad_up){
+                forward = -0.25;
+            }
+            if(gamepad1.dpad_down){
+                forward = 0.25;
+            }
+            if(gamepad1.dpad_left){
+                strafing = 0.45;
+            }
+            if(gamepad1.dpad_right){
+                strafing = -0.45;
+            }
+
+            //Strafing
+            double max = Math.max(Math.abs(forward - strafing - turning), Math.max(Math.abs(forward + strafing - turning), Math.max(Math.abs(forward + strafing + turning), Math.abs(forward - strafing + turning))));
+            if(max <  robot.maxSpeed){
+                robot.setPower(forward - strafing - turning, forward + strafing - turning, forward + strafing + turning, forward - strafing + turning);
+            } else {
+                double scaleFactor = max / robot.maxSpeed;
+                robot.setPower(forward - strafing - turning * scaleFactor, forward + strafing - turning * scaleFactor, forward + strafing + turning * scaleFactor, forward - strafing + turning * scaleFactor);
+            }
+
+            //Precision turning
+            if(gamepad1.right_bumper){
+                robot.setPower(0.45, 0.45, -0.45, -0.45);
+            }
+            if(gamepad1.left_bumper){
+                robot.setPower(-0.45, -0.45, 0.45, 0.45);
+            }
+
+            //Turret Motor
+
+            //Cone Claw
+            if (gamepad2.x && !xPressed) {
+                xPressed = true;
+                if (xKey) {
+                    robot.cc.setPosition(null);
+                    xKey = false;
+                } else {
+                    robot.cc.setPosition(null);
+                    xKey = true;
+                }
+            } else {
+                xPressed = false;
+            }
+
+
+        }
+
+        /*//Turret Motor Spin
+        public void spinTurret() {
+            if (gamepad2.right_stick_x = 1) {
+            robot.tm.setPower();
+            }
+        } */
+    }
+}
