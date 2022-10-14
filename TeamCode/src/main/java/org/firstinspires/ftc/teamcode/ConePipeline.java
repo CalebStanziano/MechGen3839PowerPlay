7cb05142cb1cf153fe811497fe8dba40dd68620f
CalebStanziano <90347719+CalebStanziano@users.noreply.github.com>
2022-10-14 11:12:19 -0400

    10/14

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConePipeline.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class ConePipeline extends OpenCvPipeline{
+    private Mat workingMatrix = new Mat();
+    public String position = "LEFT";
+
+    public ConePipeline(){
+
+    }
+
+    public final Mat processFrame(Mat input){
+        input.copyTo(workingMatrix);
+
+        if(workingMatrix.empty()){
+            return input;
+        }
+        Mat matCenter = workingMatrix.submat(120,150,10,50);
+
+        Imgproc.rectangle(workingMatrix, new Rect(10, 120, 40, 30), new Scalar(0,255,0));
+
+        //0 =red, 1 = green, 2 = blue
+        double centerTotal =Core.sumElems(matCenter).val[2];
+        centerTotal /= matCenter.cols() * matCenter.rows();
+
+        //logic here:
+
+
+        return workingMatrix;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionAuto.java
@@ -0,0 +1,265 @@
+package org.firstinspires.ftc.teamcode.MechanicalGeniuses3839;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.ConePipeline;
+import org.firstinspires.ftc.teamcode.Hardware;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous (name = "Vision Auto")
+public class VisionAuto extends LinearOpMode {
+    OpenCvCamera webCam;
+    private ConePipeline detector;
+    private String position = "LEFT";
+    Hardware robot = Hardware.getInstance();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode(){
+        robot.init(hardwareMap);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(  "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        detector = new ConePipeline();
+        webCam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "wc1"),cameraMonitorViewId);
+        webCam.openCameraDevice();
+        FtcDashboard.getInstance().startCameraStream(webCam, 0);
+        webCam.setPipeline(detector);
+        webCam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_RIGHT);
+
+        while (!isStarted()){
+            position = detector.position;
+            telemetry.addData("position", position);
+        }
+        /**add code here **/
+
+        /*if(position.equals("LEFT")){
+            robot.alsf.setTargetPosition(-480);
+            robot.cS.setPosition(.65);
+        }
+        if(position.equals("CENTER")){
+            robot.alsf.setTargetPosition(-1330);
+            robot.cS.setPosition(.65);
+        }
+        if(position.equals("RIGHT")){
+            robot.alsf.setTargetPosition(-1910);
+            robot.cS.setPosition(.65);
+        }
+        //forward(1, 1)
+         */
+
+    }
+    public void forward(double distanceMoving, double speedMoving) {
+
+        double wheelCircumference = 4 * Math.PI;
+        double wheelMotor = 560;
+        double ticks = (distanceMoving * (wheelMotor / wheelCircumference));
+        robot.setPower(0, 0, 0, 0);
+
+        robot.rf.setTargetPosition((int) Math.round(ticks));
+        robot.lf.setTargetPosition((int) Math.round(ticks));
+        robot.rb.setTargetPosition((int) Math.round(ticks));
+        robot.lb.setTargetPosition((int) Math.round(ticks));
+
+        robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.setPower(speedMoving, speedMoving, speedMoving, speedMoving);
+
+        while(opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 < ticks || opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 > ticks){
+
+        }
+        robot.setPower(0,0,0,0);
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+
+    public void diagonal(double distance, double speed, String direction){
+        double wheelCircumference = 4 * Math.PI;
+        double wheelMotor = 560;
+        double ticks = (distance * (wheelMotor / wheelCircumference));
+        robot.setPower(0, 0, 0, 0);
+        if("left/up".equals(direction)){
+
+            robot.rf.setTargetPosition((int) Math.round(ticks));
+            robot.lf.setTargetPosition((int) Math.round(ticks));
+            robot.rb.setTargetPosition((int) Math.round(ticks));
+            robot.lb.setTargetPosition((int) Math.round(ticks));
+
+            robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.setPower(speed, 0, 0, speed);
+            //robot.rf.setPower(speed);
+            //robot.lb.setPower(speed);
+
+            while(opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 < ticks || opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 > ticks){
+
+            }
+            robot.setPower(0,0,0,0);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        if("left/down".equals(direction)){
+
+            robot.rf.setTargetPosition((int) Math.round(ticks));
+            robot.lf.setTargetPosition((int) Math.round(ticks));
+            robot.rb.setTargetPosition((int) Math.round(ticks));
+            robot.lb.setTargetPosition((int) Math.round(ticks));
+
+            robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.setPower(0, -speed, -speed, 0);
+            //robot.rb.setPower(-speed);
+            //robot.lf.setPower(-speed);
+
+            while(opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 < ticks || opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 > ticks){
+
+            }
+            robot.setPower(0,0,0,0);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        if("right/up".equals(direction)){
+
+            robot.rf.setTargetPosition((int) Math.round(ticks));
+            robot.lf.setTargetPosition((int) Math.round(ticks));
+            robot.rb.setTargetPosition((int) Math.round(ticks));
+            robot.lb.setTargetPosition((int) Math.round(ticks));
+
+            robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.setPower(0, speed, speed, 0);
+            //robot.rb.setPower(speed);
+            //robot.lf.setPower(speed);
+
+            while(opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 < ticks || opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 > ticks){
+
+            }
+            robot.setPower(0,0,0,0);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        if("right/down".equals(direction)){
+
+            robot.rf.setTargetPosition((int) Math.round(ticks));
+            robot.lf.setTargetPosition((int) Math.round(ticks));
+            robot.rb.setTargetPosition((int) Math.round(ticks));
+            robot.lb.setTargetPosition((int) Math.round(ticks));
+
+            robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.setPower(-speed,0, 0, -speed);
+            //robot.rf.setPower(-speed);
+            //robot.lb.setPower(-speed);
+
+            while(opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 < ticks || opModeIsActive() && (robot.rf.getCurrentPosition()) + 10 > ticks){
+
+            }
+            robot.setPower(0,0,0,0);
+
+            robot.rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+    }
+
+    public void turning(double degrees) {
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        double currentAngle = robot.gyro.getAngularOrientation().firstAngle;
+        double finalAngle = currentAngle + degrees;
+
+        if (finalAngle > 180) {
+            finalAngle -= 360;
+        } else if (finalAngle < -180) {
+            finalAngle += 360;
+        }
+        if (degrees >= 0) {
+            double errorOfDegree = degrees;
+            while (Math.abs(errorOfDegree) > 3) {
+                robot.setPower(-0.0055 * errorOfDegree, -0.0055 * errorOfDegree, 0.0055 * errorOfDegree, 0.0055 * errorOfDegree);
+                errorOfDegree = finalAngle - robot.gyro.getAngularOrientation().firstAngle;
+                if (errorOfDegree > 180) {
+                    errorOfDegree -= 360;
+                } else if (errorOfDegree < -180) {
+                    errorOfDegree += 360;
+                }
+                errorOfDegree = Math.abs(errorOfDegree);
+            }
+        } else {
+            double errorOfDegree = degrees;
+            while (Math.abs(errorOfDegree) > 3) {
+                robot.setPower(0.0055 * errorOfDegree, 0.0055 * errorOfDegree, -0.0055 * errorOfDegree, -0.0055 * errorOfDegree);
+                errorOfDegree = finalAngle - robot.gyro.getAngularOrientation().firstAngle;
+                if (errorOfDegree > 180) {
+                    errorOfDegree -= 360;
+                } else if (errorOfDegree < -180) {
+                    errorOfDegree += 360;
+                }
+                errorOfDegree = Math.abs(errorOfDegree);
+            }
+        }
+        robot.setPower(0,0,0,0);
+    }
+}
