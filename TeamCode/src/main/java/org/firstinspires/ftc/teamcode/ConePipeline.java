5ff6bf584442b9f7d82b62f7d344b7b0a273f7e1
CalebStanziano <90347719+CalebStanziano@users.noreply.github.com>
2022-10-20 10:34:23 -0400

    Open CV now added right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConePipeline.java
@@ -5,7 +5,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.easyopencv.OpenCvPipeline;
 
 public class ConePipeline extends OpenCvPipeline{
     private Mat workingMatrix = new Mat();
