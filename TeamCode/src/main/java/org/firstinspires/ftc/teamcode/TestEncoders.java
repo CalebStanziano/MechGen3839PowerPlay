95d226e96ba82109e6246fb9504926ce4b7ead06
CalebStanziano <90347719+CalebStanziano@users.noreply.github.com>
2022-11-11 20:02:18 -0500

    11/11

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestEncoders.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp (name = "Encoder Test")
 public class TestEncoders extends LinearOpMode {
@@ -11,15 +12,37 @@ public class TestEncoders extends LinearOpMode {
         robot.init(hardwareMap);
         waitForStart();
         int position  = 0;
+        robot.lm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         boolean aPressed = false;
         boolean yPressed = false;
+        //robot.lm.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        robot.lm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         while(opModeIsActive()){
+
             //y to increase
-            if(gamepad1.y && !yPressed){
+            if(gamepad1.y){
+                position += 1;
+                robot.lm.setTargetPosition(position);
+                telemetry.addData("Position", position);
+                telemetry.update();
+                robot.cc.setPosition(.1);
+                robot.lm.setPower(.04);
+            }
+            if(gamepad1.a){
+                position -= 1;
+                robot.lm.setTargetPosition(position);
+                telemetry.addData("Position", position);
+                telemetry.update();
+                robot.lm.setPower(-.04);
+            }
+
+           /* if(gamepad1.y && !yPressed){
                 yPressed = true;
                 position += 10;
                 robot.lm.setTargetPosition(position);
+                telemetry.addData("Position", position);
+                telemetry.update();
             } else {
                 yPressed = false;
             }
@@ -28,9 +51,13 @@ public class TestEncoders extends LinearOpMode {
                 aPressed = true;
                 position -= 10;
                 robot.lm.setTargetPosition(position);
+                telemetry.addData("Position", position);
+                telemetry.update();
             } else {
                 aPressed = false;
             }
+
+            */
         }
     }
 }
