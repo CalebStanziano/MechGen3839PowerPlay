a429bca40086a802c90ab3ee3e6f6c29b43aa860
FierceFlame6098 <hegdeankit7@gmail.com>
2022-12-02 14:30:12 -0500

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teachAuto.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (name = "Teach Auto")
+public class teachAuto extends LinearOpMode {
+    Hardware robot = Hardware.getInstance();
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        // initialize
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        //forward less 1 second
+        while (runtime.seconds() < 1) {
+            robot.setPower(0.4, 0.4, 0.4, 0.4);
+        }
+        while (runtime.seconds() > 1) {
+            robot.setPower(0, 0, 0, 0);
+        }
+        runtime.reset();
+        //forward again 1
+    }
+}
+
