34233ae6ce99d2117bc1b5108bd20901a3beaa80
Bosniankid <irfan.bakir.ljubuncic@gmail.com>
2023-02-10 15:47:58 -0500

    Trash 2.0 (2/10)
    All motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware0.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware0.java
@@ -13,43 +13,61 @@ public class Hardware0 {
     public Servo servo1;
 
     private static Hardware0 myInstance = null;
+
     public static Hardware0 getInstance() {
         if (myInstance == null) {
             myInstance = new Hardware0();
         }
-            return myInstance;
+        return myInstance;
     }
 
-    public void init (HardwareMap hwMap){
+    public void init(HardwareMap hwMap) {
+
         // Right Front Motor
         try {
             rf = hwMap.get(DcMotor.class, "rf");
             rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             rf.setPower(0);
-        }
-        catch (Exception p_exception) {
+        } catch (Exception p_exception) {
             lf = null;
         }
+
         // Left Front Motor
         try {
             lf = hwMap.get(DcMotor.class, "lf");
             lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             lf.setPower(0);
-        }
-        catch (Exception p_exception) {
+        } catch (Exception p_exception) {
             lf = null;
         }
+
+        // Right Back Motor
+        try {
+            rb = hwMap.get(DcMotor.class, "rb");
+            rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rb.setPower(0);
+        } catch (Exception p_exception) {
+            rb = null;
+        }
+
+        // Left Back Motor
+        try {
+            lb = hwMap.get(DcMotor.class, "lb");
+            lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            lb.setPower(0);
+        } catch (Exception p_exception) {
+            lb = null;
+        }
+
+        // Servo 1
         try {
             servo1 = hwMap.get(Servo.class, "servo1");
-        }
-        catch (Exception p_exception) {
+        } catch (Exception p_exception) {
             servo1 = null;
         }
     }
-}
-
-
-
 }
