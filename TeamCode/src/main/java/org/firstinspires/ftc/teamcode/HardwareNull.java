b07d9148d57a07d392ec64e2c356f29f6f4249ac
Bosniankid <irfan.bakir.ljubuncic@gmail.com>
2023-02-15 15:23:15 -0500

    tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareNull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareNull.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 public class HardwareNull {
     public DcMotor rf;
@@ -20,6 +21,8 @@ public class HardwareNull {
         }
         return myInstance;
     }
+
+    public double maxSpeed = 0.75;
 
     public void init(HardwareMap hwMap) {
 
@@ -68,6 +71,20 @@ public class HardwareNull {
             servo1 = hwMap.get(Servo.class, "servo1");
         } catch (Exception p_exception) {
             servo1 = null;
+        }
+    }
+    public void setPower (double fr, double br, double fl, double bl) {
+        if (rf != null) {
+            rf.setPower(Range.clip(fr, -maxSpeed, maxSpeed));
+        }
+        if (rb != null) {
+            rb.setPower(Range.clip(br, -maxSpeed, maxSpeed));
+        }
+        if (lf != null) {
+            lf.setPower(Range.clip(fl, -maxSpeed, maxSpeed));
+        }
+        if (lb != null) {
+            lb.setPower(Range.clip(bl, -maxSpeed, maxSpeed));
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/optele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/optele.java
@@ -5,5 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp (name = "tele")
 public class optele extends LinearOpMode {
+    HardwareNull robot = HardwareNull.getInstance();
+    public void runOpMode() throws InterruptedException {
 
+    }
 }
