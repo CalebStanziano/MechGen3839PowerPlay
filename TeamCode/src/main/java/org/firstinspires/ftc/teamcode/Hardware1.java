8621a6431b916bfa2a97cf0a528b1bc98a275183
Patryk <125088422+Patrkkuk@users.noreply.github.com>
2023-02-17 16:18:09 -0500

    set hardware class and defined motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware1.java
@@ -3,66 +3,94 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-//patryk kukula is black
+import com.qualcomm.robotcore.util.Range;
+
 public class Hardware1 {
+
     public DcMotor rf;
     public DcMotor lf;
     public DcMotor lb;
-
     public DcMotor rb;
-    public Servo test;
-            private static Hardware1 myInstance =null;
-    public static Hardware1 getInstance(){
-        if(myInstance ==null){
-            myInstance =new Hardware1 ();
-        }return myInstance;
+    //defined motors
+    public Servo claw;
+    //defined servo
+    public static Hardware1 myInstance=null;
+    // idk
+    public double MaxSpeed=1;
+    //setting the maxspeed to 1-double is decimal nums
+    private Hardware1(){
     }
-    public void init (HardwareMap hwMap){
-        //right front motor
-        try{
-            rf =hwMap.get(DcMotor.class,"rf");
+    //refers to itself, makes the robot an object, defines private parts
+    public static Hardware1 getInstance(){
+        if(myInstance==null){
+        myInstance= new Hardware1();
+        }
+        return myInstance;
+    }
+    public void init(HardwareMap hwMap) {
+        //initialize hardware map, robot makes an idea of what its doing
+        try {
+            rf = hwMap.get(DcMotor.class, "rf");
+            //checking if arguement is true, adding rfmotor into hwmap
             rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             rf.setPower(0);
-        } catch(Exception p_exception){
-            rf =null;
+
+        } catch (Exception p_exception) {
+            rf = null;
         }
-        // left front motor
-        try{
-            lf =hwMap.get(DcMotor.class,"lf");
+        try {
+            lf = hwMap.get(DcMotor.class, "lf");
+            //checking if arguement is true, adding lfmotor into hwmap
             lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             lf.setPower(0);
-        } catch(Exception p_exception){
-            lf =null;
+
+        } catch (Exception p_exception) {
+            lf = null;
         }
-        //left back Motor
-        try{
-            lb =hwMap.get(DcMotor.class,"lb");
+        try {
+            rb = hwMap.get(DcMotor.class, "rb");
+            //checking if arguement is true, adding rbmotor into hwmap
+            rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rb.setPower(0);
+
+        } catch (Exception p_exception) {
+            rb = null;
+        }
+        try {
+            lb = hwMap.get(DcMotor.class, "lb");
+            //checking if arguement is true, adding lbmotor into hwmap
             lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             lb.setPower(0);
-        } catch(Exception p_exception){
-            lb =null;
-        }
-        //right back Motor
-        try{
-            rb =hwMap.get(DcMotor.class,"rb");
-            rb .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            rb.setPower(0);
-        } catch(Exception p_exception){
-            rb =null;
-        }
-        // test servo
-        try{
-            test=hwMap.get(Servo.class,"test");
 
+        } catch (Exception p_exception) {
+            lb = null;
         }
-        catch(Exception p_exception){
-            test =null;
+        try {
+            claw = hwMap.get(Servo.class, "claw");
+        } catch (Exception p_exception) {
+            claw = null;
         }
+
+
     }
-    //mecanum drivetrain
+    public void setPower(double fr, double br, double fl, double bl){
+        if(rf!=null){
+            rf.setPower(Range.clip(fr,-MaxSpeed,MaxSpeed));
+        }
+        if(lf!=null){
+            lf.setPower(Range.clip(fl,-MaxSpeed,MaxSpeed));
+        }
+        if(rb!=null){
+            rb.setPower(Range.clip(br,-MaxSpeed,MaxSpeed));
+        }
+        if(lb!=null){
+            lb.setPower(Range.clip(bl,-MaxSpeed,MaxSpeed));
+        }
+        
+    }
 
 }
