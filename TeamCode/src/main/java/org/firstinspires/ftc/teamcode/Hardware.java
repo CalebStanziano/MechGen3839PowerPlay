daf0dd5d9f09b9a2e976f6539b3e9d8ec577db6c
CalebStanziano <90347719+CalebStanziano@users.noreply.github.com>
2023-04-28 16:55:21 -0400

    4/28

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -4,13 +4,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Hardware {
-
-    public DcMotor rf;
-    public DcMotor lf;
     public DcMotor rb;
     public DcMotor lb;
 
-    public DcMotor liftMotor;
+
     private static Hardware myInstance = null;
     public static Hardware getInstance(){
         if(myInstance == null){
@@ -24,14 +21,27 @@ public class Hardware {
 
     public void init(HardwareMap hwMap){
 
-    //Right Motor
+    //Right Back Motor
         try {
-            rf = hwMap.get(DcMotor.class, "rf");
-            rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            rf.setPower(0);
+            rb = hwMap.get(DcMotor.class, "rb");
+            rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rb.setPower(0);
         } catch(Exception p_exception){
-            rf = null;
+            rb = null;
         }
+    //Left Back Motor
+        try {
+            lb = hwMap.get(DcMotor.class, "lb");
+            lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            lb.setPower(0);
+        } catch(Exception p_exception){
+            rb = null;
+        }
+    }
+    public void forward(double br,double bl){
+        
+
     }
 }
