5fe9cd46a9cd360aee6cc6218aaa9b742b465cda
CalebStanziano <90347719+CalebStanziano@users.noreply.github.com>
2023-05-12 15:33:37 -0400

    5/12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMini.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMini.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,24 +11,41 @@ public class AutoMini extends LinearOpMode {
     Hardware robot = Hardware.getInstance();
     private ElapsedTime runtime = new ElapsedTime();
 
+    RevColorSensorV3 i0;
+
     public void runOpMode(){
         robot.init(hardwareMap);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        try{
+            i0 = hardwareMap.get(RevColorSensorV3.class, "i0");
+        } catch(Exception p_exception) {
+            i0 = null;
+        }
+        
         waitForStart();
         runtime.reset();
         runtime.startTime();
         //Add Code Here:
+        forward(36, 0.75);
+
+        if(i0.red() > 95){
+            forward(108, 0.75);
+            turning(90);
+            forward(48, 0.75);
+        } else {
+            forward(108, 0.75);
+        }
 
     }
     //Methods here:
-    public void forward(double distanceMoving, double speedMoving){
-        //in inches
+    public void forward(double distanceMoving, double speedMoving) {
+
         double wheelCircumference = 4 * Math.PI;
         double wheelMotor = 560;
-        double ticks = (distanceMoving *(wheelMotor / wheelCircumference));
-        robot.setPower(0,0,0,0);
+        double ticks = (distanceMoving * (wheelMotor / wheelCircumference));
+        robot.setPower(0, 0, 0, 0);
         double startPosRf = robot.rf.getCurrentPosition();
         double startPosRb = robot.rb.getCurrentPosition();
         double startPosLf = robot.lf.getCurrentPosition();
@@ -37,7 +55,9 @@ public class AutoMini extends LinearOpMode {
         double PosRb = Math.abs(startPosRb - Math.round(startPosRb));
         double PosLf = Math.abs(startPosLf - Math.round(startPosLf));
         double PosLb = Math.abs(startPosLb - Math.round(startPosLb));
- //left off here Patty
+
+
+
         robot.rf.setTargetPosition((int) Math.round(ticks));
         robot.lf.setTargetPosition((int) Math.round(ticks));
         robot.rb.setTargetPosition((int) Math.round(ticks));
@@ -52,5 +72,65 @@ public class AutoMini extends LinearOpMode {
         robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.setPower(-speedMoving, -speedMoving, -speedMoving, -speedMoving);
+
+        while(opModeIsActive() && (robot.rf.isBusy())){
+            double robotPos = robot.rf.getCurrentPosition();
+            telemetry.addData("Position of Bot:", robotPos );
+        }
+        robot.setPower(0,0,0,0);
+
+
     }
+
+    public void turning (double degrees) {
+
+        robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        double currentAngle = robot.gyro.getAngularOrientation().firstAngle;
+        double finalAngle = currentAngle + degrees;
+
+
+        if (finalAngle >= 180) {
+            finalAngle -= 360;
+        } else if (finalAngle < -180) {
+            finalAngle += 360;
+        }
+        if (degrees >= 0) {
+            double errorOfDegree = degrees;
+            while (Math.abs(errorOfDegree) > 3) {
+                robot.setPower(-0.0055 * errorOfDegree, -0.0055 * errorOfDegree, 0.0055 * errorOfDegree, 0.0055 * errorOfDegree);
+                errorOfDegree = finalAngle - robot.gyro.getAngularOrientation().firstAngle;
+                if (errorOfDegree > 180) {
+                    errorOfDegree -= 360;
+                } else if (errorOfDegree < -180) {
+                    errorOfDegree += 360;
+                }
+                errorOfDegree = Math.abs(errorOfDegree);
+            }
+        } else {
+            double errorOfDegree = degrees;
+            while (Math.abs(errorOfDegree) > 3) {
+                robot.setPower(0.0055 * errorOfDegree, 0.0055 * errorOfDegree, -0.0055 * errorOfDegree, -0.0055 * errorOfDegree);
+                errorOfDegree = finalAngle - robot.gyro.getAngularOrientation().firstAngle;
+                if (errorOfDegree > 180) {
+                    errorOfDegree -= 360;
+                } else if (errorOfDegree < -180) {
+                    errorOfDegree += 360;
+                }
+                errorOfDegree = Math.abs(errorOfDegree);
+            }
+        }
+        robot.setPower(0,0,0,0);
+    }
+
 }
