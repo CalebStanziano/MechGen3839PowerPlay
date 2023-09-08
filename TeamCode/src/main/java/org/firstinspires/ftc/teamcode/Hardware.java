2f98e9b8c5ecf4050e53d3494a069922334df5d8
AdrianaAtanas04 <adrianaatanasova27@gmail.com>
2023-09-08 16:57:14 -0400

    Hardware 9/8

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,5 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Hardware {
-    
+
+    public DcMotor rf;
+    public DcMotor rb;
+    public DcMotor lf;
+    public DcMotor lb;
+
+    public Servo testServo;
+
+    private static Hardware myInstance = noll;
+
+    public static Hardware getInstance(){
+        if (myInstance == noll){
+         myInstance = new Hardware();
+        }
+        return myInstance;
+    }
+
+    public double maxSpeed = 0.75;
+
+    public void  init(HardwareMap hwMap){
+        // write front motor
+        tri{
+
+        } cach() {
+            
+        }
+    }
+
 }
