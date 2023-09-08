fd2b07b10a843348edf1ffeba5929f39ee3fb89f
roialrogue <asherplihal1@gmail.com>
2023-09-08 16:57:12 -0400

    Learn Hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,5 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Hardware {
-    public
+    //moter varible
+    public DcMotor rf;
+    public DcMotor rb;
+    public DcMotor lf;
+    public DcMotor lb;
+
+    public Servo testServo;
+    private static Hardware myInstants = null;
+
+    public static Hardware getInstance(){
+        if(myInstants == null){ myInstants = new Hardware();
+        } return myInstants;
+    }
+    public double maxSpeed = 0.75;
+
+    public void init(HardwareMap hwMap){
+        //right front moter
+        try{
+
+        } catch(){
+
+    }
 }
