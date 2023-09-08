1076715bb74bce7d9fcf5a50803eca3082db03ce
CalebStanziano <Calebstanziano@gmail.com>
2023-09-08 16:57:12 -0400

    hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,5 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Hardware {
-    public 
+    //define motor
+    public DcMotor rf;
+    public DcMotor rb;
+    public DcMotor lf;
+    public DcMotor lb;
+
+    public Servo testServo;
+
+    private static Hardware myInstance = null;
+
+    public static Hardware getInstance(){
+        if(myInstance == null){
+            myInstance = new Hardware();
+        }
+        return myInstance;
+    }
+
+    public double maxSpeed = 0.75;
+
+    public void init(HardwareMap hwMap){
+        //right front motor
+        try{
+
+        } catch() {
+            
+        }
+    }
+
 }
